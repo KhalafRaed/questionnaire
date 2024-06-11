47b501025a4ed960c901e06cdaa50c4a217a3e22
RaedAli <ateam.rali@insightpartners.com>
2024-06-11 23:56:28 +0300

    Configure Shadcn + Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Button } from '@/components/ui/button.tsx';
 function App() {
   return (
     <>
-      <Button>Hey</Button>
+      <div className="m-10">
+        <Button variant="destructive">Hey</Button>
+      </div>
     </>
   );
 }
